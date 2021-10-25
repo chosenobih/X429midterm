0a9283456761332da3895a1dc69cd07afcea7fbf
rysterman34 <ryanpapetti@gmail.com>
2021-10-24 21:56:11 -0700

    preparing for running test data'

---
+++ scripts/model_predictions.py
@@ -0,0 +1,32 @@
+import logging, joblib, numpy as np
+from typing import Dict, Tuple
+from keras.models import load_model
+
+from ensemble_evaluation import EnsembleModel
+
+logging.basicConfig(filename='../logs/test_model_predictions.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', filemode='w')
+
+allow_pickle_flag = True
+
+TEST_DATA = np.load('../data/combined_data_test.npy', allow_pickle=allow_pickle_flag)
+results_dir = '../results'
+YIELD_SCALER = joblib.load(results_dir + '/yield_scaler.sav')
+
+
+
+OG_MODEL = load_model('../results/lstm_attention_recent_model')
+LSTM_MODEL = load_model('../results/lstm_recent_model')
+LSTM_DEEP_MODEL = load_model('../results/deep_lstm_recent_model')
+
+
+ryan_ensemble = EnsembleModel([OG_MODEL, LSTM_MODEL, LSTM_DEEP_MODEL], YIELD_SCALER)
+test_predictions = ryan_ensemble.predict(TEST_DATA, batch_size=512)
+ryan_ensemble.write_predictions(test_predictions)
+
+
+
+
+
+
+
+
